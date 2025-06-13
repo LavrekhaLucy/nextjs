e702b336f62d1a0939cb03832d7321b530e041c0
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-06-13 09:05:32 +0300

    hw/3(fixed api rout)

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,17 +1,23 @@
 import {NextRequest, NextResponse} from 'next/server';
-import {carsValidator} from '@/validator/carsValidator';
-import {sendCars} from '@/services/api.service';
+import {sendCars} from "@/services/api.service";
 
 export async function POST(req: NextRequest) {
 
     const body = await req.json();
-    const { error, value } = carsValidator.validate(body, { abortEarly: false });
 
-    if (error) {
-        return NextResponse.json({ error: error.details.map(e => e.message) }, { status: 400 });
+    if (!body.brand || typeof body.brand !== 'string') {
+        return NextResponse.json({ error: 'Brand is required' }, { status: 400 });
+    }
+    if (!body.year || typeof body.year !== 'number') {
+        return NextResponse.json({ error: 'Year is required and must be a number' }, { status: 400 });
+    }
+    if (!body.price || typeof body.price !== 'number') {
+        return NextResponse.json({ error: 'Price is required and must be a number' }, { status: 400 });
     }
 
-    await sendCars(value.brand,value.year,value.price);
 
-    return NextResponse.json({ success: true });
+    await sendCars(body.brand, body.year, body.price);
+        return NextResponse.json({ success: true });
+
 }
+
